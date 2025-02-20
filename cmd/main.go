d4ec002a8679dfbbaafc0d97a35a82d540c17029
morelucks <luckykamshak@gmail.com>
2025-02-20 23:52:35 +0100

    _fixes

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,10 @@ func main() {
 	// Create a new stack
 	stack := types.NewStack()
 
-	// Push elements
+	//... Push elements ..//
 	stack.Push(22)
 	
 	fmt.Println("Popped:", stack.Pop())
 
-	// Uncommenting the next line will cause a panic (stack underflow)
-	// fmt.Println("Popped:", stack.Pop())
+	
 }
